0292d987467534b40838e2f2de4590b60c510961
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-09-24 17:34:58 +0300

    handling async code with redux-thunl

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -34,11 +34,20 @@ export const subtract = (value)=>{
     }
 };
 
-export const storeResult = (res)=>{
+export const saveResult = (res)=>{
     return{
         type:STORE_RESULT,
         result:res
     }
+}
+export const storeResult = (res)=>{
+    return dispatch=>{
+        setTimeout(()=>{
+            dispatch(saveResult(res));
+        },2000);
+    }
+    
+    
 };
 
 export const deleteResult = (resElId)=>{
