9670ce5b8635506e2a244156c7dfdb912a860916
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-09-25 18:52:07 +0300

    even more learner Switch cases in the reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,17 @@ const initialState = {
     results: []
 };
 
+const deleteResult=(state, action)=>{
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state,{results: updatedArray});
+}
+
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
-            return updateObject(state,{results: state.results.concat({id: new Date(), value: action.result})});
-              
+            return updateObject(state,{results: state.results.concat({id: new Date(), value: action.result})});             
         case actionTypes.DELETE_RESULT:
-
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state,{results: updatedArray});
+            return  deleteResult(state,action);
             
     }
     return state;
